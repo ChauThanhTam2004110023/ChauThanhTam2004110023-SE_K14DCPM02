9fcd3af7d0dba7c2dcbf5b21820ef45ea4eb5591
ChauThanhTam2004110023 <tamc5985@gmail.com>
2022-04-26 12:54:12 +0700

    Add Class

---
+++ src/Buoi5/Car.java
@@ -0,0 +1,12 @@
+package Buoi5;
+
+public class Car {
+    String name;
+    int year;
+    
+    public Car(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+    
+}
